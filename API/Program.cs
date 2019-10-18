b73cb34f60ef2a0d879526495486afc8d631d667
Joshua Calkins <joshua_calkins@comcast.com>
2019-10-18 15:22:02 -0400

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        // Method that builds and runs our application
+        {
+            var host =  CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope()){
+                var services = scope.ServiceProvider;
+                // Everytime the application is started, check to see if there's a database
+                // if there's not, create one based on migrations
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Oh snap! Looks like an error hath occured during migration my programmer lord!");
+                }
+            }
+
+            host.Run();
+        }
+        // Creates a new instance of our Website builder class
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
